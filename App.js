560dbb9a16df7c4f48bac9f99a927908cba36d15
Ricardo Olarte <raop155@hotmail.com>
2020-12-30 10:48:50 -0500

    Layout

--- App.js
+++ App.js
@@ -1,12 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+
+const Texto = ({ style }) => {
+  const [texto, setTexto] = useState('Hello World :)');
+
+  const handlePress = () => {
+    setTexto('Bye Cruel World');
+  };
+
+  return (
+    <Text style={[styles.text, style]} onPress={handlePress}>
+      {texto}
+    </Text>
+  );
+};
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style='auto' />
+      <Texto style={styles.red} />
+      <Texto style={styles.green} />
+      <Texto style={styles.blue} />
     </View>
   );
 }
@@ -15,7 +31,26 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'stretch',
+    justifyContent: 'space-evenly',
+    // flexDirection: 'row',
+  },
+  red: {
+    // flex: 1,
+    backgroundColor: '#ff0000',
+  },
+  green: {
+    // flex: 2,
+    backgroundColor: '#00ff00',
+  },
+  blue: {
+    // flex: 3,
+    backgroundColor: '#0000ff',
+  },
+  text: {
+    color: '#fff',
+    fontSize: 24,
+    // height: 100,
+    width: '50%',
   },
 });
