499992809055375e96309120db60c0081739760f
Ricardo Olarte <raop155@hotmail.com>
2020-12-30 12:41:39 -0500

    FlatList

--- App.js
+++ App.js
@@ -1,79 +1,37 @@
-import React, { useState } from 'react';
-import {
-  StyleSheet,
-  Text,
-  TextInput,
-  View,
-  Button,
-  TouchableHighlight,
-  TouchableNativeFeedback,
-  TouchableWithoutFeedback,
-  TouchableOpacity,
-  ScrollView,
-  Dimensions,
-} from 'react-native';
+import React from 'react';
+import { StyleSheet, View, FlatList, Text } from 'react-native';
 
 // const width = Dimensions.get('window').width;
 
 export default function App() {
-  const [text, setText] = useState('Welcome Back!');
-
   return (
     <View style={styles.container}>
-      <ScrollView style={styles.scrollView}>
-        <Text>Text: {text}</Text>
-        <Text>Text: {text}</Text>
-        <Text>Text: {text}</Text>
-        <Text>Text: {text}</Text>
-        <Text>Text: {text}</Text>
-        <Text>Text: {text}</Text>
-        <Text>Text: {text}</Text>
-        <Text>Text: {text}</Text>
-        <Text>Text: {text}</Text>
-        <Text>Text: {text}</Text>
-        <Text>Text: {text}</Text>
-        <Text>Text: {text}</Text>
-        <Text>Text: {text}</Text>
-        <Text>Text: {text}</Text>
-        <Text>Text: {text}</Text>
-        <Text>Text: {text}</Text>
-        <Text>Text: {text}</Text>
-        <Text>Text: {text}</Text>
-        <Text>Text: {text}</Text>
-        <Text>Text: {text}</Text>
-        <Text>Text: {text}</Text>
-        <Text>Text: {text}</Text>
-        <Text>Text: {text}</Text>
-        <Text>Text: {text}</Text>
-        <Text>Text: {text}</Text>
-        <Text>Text: {text}</Text>
-        <Text>Text: {text}</Text>
-        <Text>Text: {text}</Text>
-        <Text>Text: {text}</Text>
-        <Text>Text: {text}</Text>
-        <Text>Text: {text}</Text>
-        <Text>Text: {text}</Text>
-        <Text>Text: {text}</Text>
-        <Text>Text: {text}</Text>
-        <Text>Text: {text}</Text>
-        <Text style={{ textAlign: 'center' }}>Text: {text}</Text>
-        <TextInput
-          style={styles.input}
-          placeholder='Insert a text'
-          onChangeText={(t) => setText(t)}
-          value={text}
-        />
-        <TouchableWithoutFeedback
-          style={styles.touchableOpacity}
-          onPress={() => {
-            setText('Pressed change text');
-          }}
-        >
-          <View style={styles.view}>
-            <Text>Accept</Text>
-          </View>
-        </TouchableWithoutFeedback>
-      </ScrollView>
+      <FlatList
+        data={[
+          { key: '1', name: 'Name 1' },
+          { key: '2', name: 'Name 2' },
+          { key: '3', name: 'Name 3' },
+          { key: '4', name: 'Name 4' },
+          { key: '5', name: 'Name 5' },
+          { key: '6', name: 'Name 6' },
+          { key: '7', name: 'Name 7' },
+          { key: '8', name: 'Name 8' },
+          { key: '9', name: 'Name 9' },
+          { key: '10', name: 'Name 10' },
+          { key: '11', name: 'Name 11' },
+          { key: '12', name: 'Name 12' },
+          { key: '13', name: 'Name 13' },
+          { key: '14', name: 'Name 14' },
+          { key: '15', name: 'Name 15' },
+        ]}
+        renderItem={({ item }) => {
+          return (
+            <Text style={styles.item} key={item.key}>
+              {item.name}
+            </Text>
+          );
+        }}
+      />
     </View>
   );
 }
@@ -82,23 +40,14 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
+    alignItems: 'stretch',
   },
-  view: {
-    height: 50,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  touchableOpacity: {
-    backgroundColor: '#999',
-  },
-  input: {
-    height: 40,
-    borderBottomColor: '#999',
+  item: {
+    padding: 10,
+    fontSize: 22,
+    // height: 50,
+    borderBottomColor: '#ddd',
     borderBottomWidth: 1,
-    width: '100%',
-  },
-  scrollView: {
-    width: Dimensions.get('window').width,
+    // width: '100%',
   },
 });
