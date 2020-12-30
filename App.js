182d84bf39666ba4fbcd1e046eb444681c60346b
Ricardo Olarte <raop155@hotmail.com>
2020-12-30 14:57:41 -0500

    Image

--- App.js
+++ App.js
@@ -1,57 +1,23 @@
-import React, { useEffect, useState } from 'react';
-import { StyleSheet, View, FlatList, Text, ActivityIndicator } from 'react-native';
+import React from 'react';
+import { StyleSheet, View, Image } from 'react-native';
 
 export default function App() {
-  const [users, setUsers] = useState([]);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then((res) => res.json())
-      .then((json) => {
-        setUsers(json);
-        setLoading(false);
-      });
-  }, []);
-
-  if (loading) {
-    return (
-      <View style={styles.center}>
-        <ActivityIndicator size='large' color='#0000ff' />
-        <Text>Cargando...</Text>
-      </View>
-    );
-  }
-
   return (
     <View style={styles.container}>
-      <FlatList
-        data={users}
-        renderItem={({ item }) => {
-          return <Text style={styles.item}>{item.name}</Text>;
-        }}
-        keyExtractor={(item) => item.id.toString()}
-      />
+      <Image style={styles.photo} source={{ uri: 'https://placekitten.com/200/200' }} />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  photo: {
+    height: 200,
+    width: 200,
+  },
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'stretch',
-    paddingTop: 20,
-  },
-  item: {
-    padding: 10,
-    fontSize: 22,
-    borderBottomColor: '#ddd',
-    borderBottomWidth: 1,
-  },
-  center: {
-    flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop: 20,
   },
 });
