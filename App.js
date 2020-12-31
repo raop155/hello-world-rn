0f4af128c13fb8de9fc95891a4dcac7e08057e09
Ricardo Olarte <raop155@hotmail.com>
2020-12-31 12:23:53 -0500

    Modal

--- App.js
+++ App.js
@@ -1,25 +1,47 @@
-import React from 'react';
-import { StyleSheet, View, ImageBackground, Text } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View, Modal, Text, Button } from 'react-native';
 
 export default function App() {
+  const [modal, setModal] = useState(false);
   return (
     <View style={styles.container}>
-      <ImageBackground style={styles.photo} source={{ uri: 'https://placekitten.com/200/200' }}>
-        <Text>lalala</Text>
-      </ImageBackground>
+      <Modal animationType='slide' transparent={true} visible={modal}>
+        <View style={styles.center}>
+          <View style={styles.content}>
+            <Text>Modal here!</Text>
+            <Button title='Close' onPress={() => setModal(!modal)} />
+          </View>
+        </View>
+      </Modal>
+      <Text>Not modal</Text>
+      <Text>Not modal</Text>
+      <Text>Not modal</Text>
+      <Text>Not modal</Text>
+      <Text>Not modal</Text>
+      <Button title='Open' onPress={() => setModal(!modal)} />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  photo: {
-    height: 200,
-    width: 200,
-  },
   container: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
     paddingTop: 20,
+  },
+  center: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0,0,0,0.3)',
+  },
+  content: {
+    backgroundColor: '#fff',
+    height: '75%',
+    width: '75%',
+    borderRadius: 15,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
